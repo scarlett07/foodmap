6e4b3e81f5727e0e41561d8ed295b484b19c0137
scarlett07 <scarlett.geo@gmail.com>
2018-01-23 09:28:00 -0600

    subiendo pryecto

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var map;
+
+var restaurantes = data.restaurantes;
+var gallery = $('#gallery');
+
+$(document).ready(initApp());
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('mapa'), {
+    center: {
+      lat: 19.4203024,
+      lng: -99.1631142
+    },
+    zoom: 15
+  });
+}
+
+function initApp() {
+  loadAllData('');
+  $('#search').keyup(function(event) {
+    // Borramos el contenido de gallery
+    gallery.empty();
+    var text = $('#search').val();
+    loadAllData(text.trim().toLowerCase());
+  });
+}
+
+function loadAllData(filter) {
+  $.each(restaurantes, function(index, value) {
+    if (value.type.includes(filter) ||
+    value.address.toLowerCase().includes(filter) ||
+    value.name.toLowerCase().includes(filter)
+  ) {
+      var div = $('<div class="col-sm-4 col-xs-4 col-md-3"></div>');
+      var a = $('<a href="#" class="thumbnail"></a>')
+      var image = $('<img src="' + value.picture + '" alt="' + value.name + '">');
+      a.append(image);
+      div.append(a);
+      gallery.append(div);
+    }
+  });
+}
---
+++ js/data.js
@@ -0,0 +1,48 @@
+var data = {
+  restaurantes: [{
+    country: 'México',
+    city: 'Mexico City',
+    name: 'Taquitos Sensuales',
+    type: 'mexicana',
+    latitude: 19.4287161,
+    longitude: -99.16875270000003,
+    address: 'Calle Tonala 30',
+    picture: './assets/images/mexicana.jpg'
+  }, {
+    country: 'México',
+    city: 'Mexico City',
+    name: 'Parrila Argentina',
+    type: 'argentina',
+    latitude: 19.4287161,
+    longitude: -99.16875270000003,
+    address: 'Calle Tonala 30',
+    picture: './assets/images/argentina.jpg'
+  }, {
+    country: 'México',
+    city: 'Mexico City',
+    name: 'Taquitos Sensuales',
+    type: 'italiana',
+    latitude: 19.4287161,
+    longitude: -99.16875270000003,
+    address: 'Calle Tonala 30',
+    picture: './assets/images/italiana.png'
+  }, {
+    country: 'México',
+    city: 'Mexico City',
+    name: 'Taquitos Poblanos',
+    type: 'mexicana',
+    latitude: 19.4287161,
+    longitude: -99.16875270000003,
+    address: 'Calle Tonala 30',
+    picture: './assets/images/poblana.jpg'
+  }, {
+    country: 'México',
+    city: 'Mexico City',
+    name: 'Taquitos Sensuales',
+    type: 'mariscos',
+    latitude: 19.4287161,
+    longitude: -99.16875270000003,
+    address: 'Calle Tonala 30',
+    picture: './assets/images/mar-y-tierra.jpg'
+  }]
+}
