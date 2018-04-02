f6ce773f5754b25bd2d6f36d304284fd15c70c4d
scarlett07 <scarlett.geo@gmail.com>
2018-04-02 10:28:53 -0500

    Marker modal

--- js/app.js
+++ js/app.js
@@ -3,20 +3,59 @@ var map;
 var restaurantes = data.restaurantes;
 var gallery = $('#gallery');
 
-//cargando funciones
-$(document).ready(initApp());
+//mapa
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getCoodinates);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+};
 
-function initApp() {
-  loadAllData('');
-  //Eventos
-  $(document).on("click", ".imageMini", loadModal);
-  $('#search').keyup(function(event) {
-    // Borramos el contenido de gallery
-    gallery.empty();
-    var text = $('#search').val();
-    loadAllData(text.trim().toLowerCase());
+const getCoodinates = position => {
+  const latitude =  position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log(latitude, longitude);
+  initMap(latitude, longitude);
+};
+
+function initMap(latitude, longitude) {
+  map = new google.maps.Map(document.getElementById('mapa'), {
+    center: {
+      lat: latitude,
+      lng: longitude
+    },
+    zoom: 15
   });
-}
+  var marker = new google.maps.Marker({
+    position: {
+      lat: latitude,
+      lng: longitude
+    },
+    map: map,
+    title: '¡Usted esta aquí!'
+  });
+//  marker.setMap(map);
+};// END map main view
+
+function drawMap(latitude, longitude) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: latitude,
+      lng: longitude
+    },
+    zoom: 15
+  });
+  var marker = new google.maps.Marker({
+    position: {
+      lat: latitude,
+      lng: longitude
+    },
+    map: map,
+    title: '¡Restaurante!'
+  }); //END marker
+}; // END map modal
+
 
 function loadAllData(filter) {
   $.each(restaurantes, function(index, value) {
@@ -24,7 +63,7 @@ function loadAllData(filter) {
     value.address.toLowerCase().includes(filter) ||
     value.name.toLowerCase().includes(filter)
   ) {
-      var div = $('<div class="imageMini col-sm-4 col-xs-6 col-md-4"></div>');
+      var div = $('<div class="imageMini col-sm-6 col-xs-12 col-md-4"></div>');
       var a = $('<a href="#" data-toggle="modal" data-target="#myModal" class="thumbnail"></a>')
       var image = $('<img src="' + value.picture + '" alt="' + value.name + '">');
       a.append(image);
@@ -32,10 +71,10 @@ function loadAllData(filter) {
       gallery.append(div);
     }
   });
-} //END loadAllData
+}; //END loadAllData
+
 
 //modal
-
 function loadModal(event){
   var datosRestaurant = event.target.alt
   console.log(datosRestaurant);
@@ -46,17 +85,21 @@ function loadModal(event){
   var selection = restaurantes
   .filter(element =>element.name==datosRestaurant);
   description.innerText= selection[0].address;
-  photo.src= selection[0].picture;
-} //END loadModal
+  drawMap(selection[0].latitude, selection[0].longitude)
+}; //END loadModal
 
+//cargando funciones
+$(document).ready(initApp());
 
-//mapa
-function initMap() {
-  map = new google.maps.Map(document.getElementById('mapa'), {
-    center: {
-      lat: 19.4203024,
-      lng: -99.1631142
-    },
-    zoom: 15
+function initApp() {
+  loadAllData('');
+  getLocation();
+  //Eventos
+  $(document).on("click", ".imageMini", loadModal);
+  $('#search').keyup(function(event) {
+    // Borramos el contenido de gallery
+    gallery.empty();
+    var text = $('#search').val();
+    loadAllData(text.trim().toLowerCase());
   });
-}
+};
